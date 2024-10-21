ed20a1ec1ad168d617cc401cd4b5a9b5680c8de0
Beno!t POLASZEK <bpolaszek@gmail.com>
2024-10-21 07:53:42 +0200

    feat: debug memory usage

---
+++ app/components/debug/DebugMemory.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="isSupported && memory">
+    <template v-if="memory">
+      <div>Used</div>
+      <div class="text-right">{{ size(memory.usedJSHeapSize) }}</div>
+
+      <div>Allocated</div>
+      <div class="text-right">{{ size(memory.totalJSHeapSize) }}</div>
+
+      <div>Limit</div>
+      <div class="text-right">{{ size(memory.jsHeapSizeLimit) }}</div>
+    </template>
+  </div>
+  <div v-else class="text-xs italic text-gray-600">
+    Performance memory API not supported
+  </div>
+</template>
+
+<script setup>
+import { useMemory } from '@vueuse/core'
+
+const size = (v) => {
+  const kb = v / 1024 / 1024
+  return `${kb.toFixed(2)} MB`
+}
+const { isSupported, memory } = useMemory()
+</script>
